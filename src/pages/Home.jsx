bfe4ef0268c5788e621a66017120aecc0e6a0d15
Berfin4615 <berfins.4615@gmail.com>
2026-01-07 04:45:33 +0300

    HomeACtionBar is added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from '../components/home/Hero.jsx';
+import HomeActionBar from '../components/home/HomeActionBar.jsx';
 
 const HomePage = () => {
     return (
         <div>
             <Hero />
+            <HomeActionBar timeLeft={{ months: 3, days: 10, hours: 9 }} />
         </div>
     );
 };
