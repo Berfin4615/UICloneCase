b68355dd4ae6243b2282de12c75c137b0c2287d7
Berfin4615 <berfins.4615@gmail.com>
2026-01-07 03:54:02 +0300

    Home Page's hero component is added.

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,32 @@
+import heroBg from '../../assets/home_hero.png';
+
+const Hero = () => {
+    return (
+        <div>
+            <section className="relative w-full h-[520px] md:h-[620px] overflow-hidden">
+                <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${heroBg})` }} />
+                    <div className="absolute inset-0 bg-cover bg-center opacity-30" />
+                    <div className="relative max-w-7xl mx-auto px-6 h-full flex items-center">
+                        <div className="text-white max-w-2xl">
+                            <h2 className="font-medium tracking-[-0.02em] leading-[1.05] text-[44px] sm:text-[52px] md:text-[68px]">
+                                Sağlık Bilimlerinde
+                                <br />
+                                Bilimsel Gelişmeler ve
+                                <br />
+                                Teknoloji Kongresi
+                            </h2>
+                            <div className="mt-16 space-y-1 text-[23px] md:text-[25px] font-normal">
+                                <div>
+                                    20–25 Ocak 2026
+                                    <br />
+                                    Nazım Hikmet Kongre ve Sanat Merkezi, Ankara
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Hero;
